d8c556f0b4a8ba9bccefa2dd48063ea43a828880
Dr Nic Williams <drnicwilliams@gmail.com>
2017-02-06 18:28:40 +1000

    fix tox warnings

--- wal_e/blobstore/remote/utils.py
+++ wal_e/blobstore/remote/utils.py
@@ -1,10 +1,9 @@
 # import gevent
-import shutil
+# import shutil
 
 from urllib.parse import urlparse
 from os.path import dirname
 import subprocess
-import tempfile
 
 # from wal_e import files
 from wal_e import log_help
@@ -25,11 +24,12 @@ def uri_put_file(creds, uri, fp, content_type=None):
     proc = subprocess.Popen([
         'ssh', '-i', creds.identity_file,
         '%s@%s' % (creds.user, creds.host),
-        'mkdir -p %s && cat > %s' % (dst_dir, dst_path)], stdin=subprocess.PIPE)
+        'mkdir -p %s && cat > %s' % (dst_dir, dst_path)],
+                            stdin=subprocess.PIPE)
 
     try:
         outs, errs = proc.communicate(input=fp.read(), timeout=15)
-    except TimeoutExpired:
+    except subprocess.TimeoutExpired:
         proc.kill()
         outs, errs = proc.communicate()
 
